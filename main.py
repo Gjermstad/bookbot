f62e394e5c4de8db004683360073ad849f4d710e
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-01-03 22:07:15 +0100

    Finished CH3 L2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 
@@ -7,7 +8,7 @@ def get_book_text(filepath):
 
 
 def print_report(filepath, num_words, chars_sorted_list):
-    print("============ BOOKBOT ============")
+    print("\n============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
@@ -16,11 +17,15 @@ def print_report(filepath, num_words, chars_sorted_list):
         if not item["char"].isalpha():
             continue
         print(f"{item['char']}: {item['num']}")
-    print("============= END ===============")
+    print("============= END ===============\n")
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("\nERROR: Filepath to a book or text-file required.")
+        print("Usage: python3 main.py <path_to_book>\n")
+        sys.exit(1)
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
